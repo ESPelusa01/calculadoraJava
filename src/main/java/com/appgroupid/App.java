0dd33d4e6fd0742fa6aff7c87f2835b799772451
Xiorad1412 <dario.salasarellano@alum.uca.es>
2022-11-24 12:00:18 +0100

    inicial

---
+++ src/main/java/com/appgroupid/App.java
@@ -0,0 +1,17 @@
+package com.appgroupid;
+
+/**
+ * Hello world!
+ *
+ *         <plugin>
+              <artifactId>maven-jar-plugin</artifactId>
+              <version>3.0.2</version>
+        </plugin>
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
